04c6fe8570d0be002fae34d0a4a0cf63d89b3d0c
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-03-30 17:31:19 -0400

    Write lora command handler module

---
+++ firmware/include/lora_handlers.h
@@ -0,0 +1,16 @@
+/**
+ * @file lora_handlers.h
+ *
+ * @brief
+ *
+ * @date 3/30/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+#ifndef ARES_LORA_HANDLERS_H
+#define ARES_LORA_HANDLERS_H
+
+void refresh_modem_id(void);
+
+#endif // ARES_LORA_HANDLERS_H
---
+++ firmware/src/lora_handlers.c
@@ -0,0 +1,73 @@
+/**
+ * @file lora_handlers.c
+ *
+ * @brief
+ *
+ * @date 3/30/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+#include <lora/lora.h>
+#include <lora/lora_backend.h>
+#include <lora/packet.h>
+#include <lora_handlers.h>
+#include <serial/frame.h>
+#include <serial/serial.h>
+#include <serial/serial_backend.h>
+#include <settings.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+
+struct modem_id {
+    uint16_t pan_id;
+    uint16_t id;
+};
+
+static struct modem_id modem_id;
+
+static void handle_start(const struct ares_lora *lora,
+                         const struct ares_packet *packet) {
+    ARG_UNUSED(lora);
+
+    const struct ares_serial *serial = ares_serial_backend_uart_get_ptr();
+    struct ares_frame frame = {
+        .type = ARES_FRAME_START,
+    };
+
+    if (packet->type == ARES_PKT_TYPE_DIRECT &&
+        (packet->destination_id != modem_id.id ||
+         packet->pan_id != modem_id.pan_id)) {
+        // not meant for us...
+        return;
+    }
+
+    frame.payload.START.destination = packet->source_id;
+    frame.payload.START.broadcast = packet->type == ARES_PKT_TYPE_BROADCAST;
+    frame.payload.START.sec = packet->payload.payload.timespec.sec;
+    frame.payload.START.ns = packet->payload.payload.timespec.nsec;
+
+    ares_serial_write_frame(serial, &frame);
+}
+
+static struct ares_lora_command commands[] = {
+    {ARES_PKT_PAYLOAD_START, handle_start},
+};
+
+static int init_lora_handlers(void) {
+    const struct ares_lora *lora = ares_lora_backend_lora_get_ptr();
+    refresh_modem_id();
+    return ares_lora_register_command_callbacks(lora, commands,
+                                                ARRAY_SIZE(commands));
+}
+SYS_INIT(init_lora_handlers, APPLICATION, 91);
+
+void refresh_modem_id(void) {
+    uint32_t pan, id;
+
+    retrieve_setting(ARES_SETTING_PANID, &pan);
+    retrieve_setting(ARES_SETTING_ID, &id);
+
+    modem_id.id = (uint16_t)id;
+    modem_id.pan_id = (uint16_t)pan;
+}
