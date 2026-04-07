9e7a6fb1d395cd8aa74923c9eb578eddf815a16f
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-04-07 13:28:21 -0400

    Update package

--- serial-driver/src/ares_lora/__init__.py
+++ serial-driver/src/ares_lora/__init__.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from .serial import LoraException, SettingId, LoraBandwidth, LoraSpreadingFactor, LoraCodingRate, LoraConfig, \
     LoraSerial, LoraLedState
 from .version import __version__
-from .dfu import AresDfu
+from .dfu import AresDfu, AresUploadStatusBase, AresImageStates, ImageManagerException
 
 __all__ = ["__version__", "LoraSerial", "LoraConfig", "LoraException", "SettingId", "LoraBandwidth",
-           "LoraSpreadingFactor", "LoraCodingRate", "LoraLedState", "AresDfu"]
+           "LoraSpreadingFactor", "LoraCodingRate", "LoraLedState", "AresDfu", "AresImageStates",
+           "AresUploadStatusBase", "ImageManagerException"]
