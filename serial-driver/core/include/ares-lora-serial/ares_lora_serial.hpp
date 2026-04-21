8e9f409e642f1896e097905d2fdb61da5409758f
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-04-21 13:22:39 -0400

    Serial driver docs

--- serial-driver/core/include/ares-lora-serial/ares_lora_serial.hpp
+++ serial-driver/core/include/ares-lora-serial/ares_lora_serial.hpp
@@ -1,7 +1,7 @@
 /**
  * @file ares_lora_serial.hpp
  *
- * @brief
+ * @brief Ares Lora serial driver for the host computer.
  *
  * @date 3/31/26
  *
@@ -32,10 +32,22 @@
 namespace py = pybind11;
 using namespace std::chrono_literals;
 
+/**
+ * @class AresTimeoutError
+ * Timeout exception class for the Ares serial driver core library.
+ */
 class AresTimeoutError : public std::exception {
   public:
+    /**
+     * Constructor.
+     * @param msg The error message.
+     */
     explicit AresTimeoutError(std::string msg) : _msg(std::move(msg)) {}
 
+    /**
+     * Retrieve the error message.
+     * @return The error message.
+     */
     [[nodiscard]] const char *what() const noexcept override {
         return _msg.c_str();
     }
@@ -44,74 +56,256 @@ class AresTimeoutError : public std::exception {
     std::string _msg;
 };
 
+/**
+ * @struct AresSerialConfigs
+ *
+ * Serial driver configurations.
+ */
 struct AresSerialConfigs {
     AresSerialConfigs() = default;
+
+    /**
+     * Construct from Python kwargs.
+     * @param kwargs Python keyword arguments.
+     */
     explicit AresSerialConfigs(const py::kwargs &kwargs);
 
+    /**
+     * The serial port.
+     */
     std::string port;
+
+    /**
+     * Serial reception timeout.
+     */
     std::chrono::milliseconds serial_timeout = 100ms;
+
+    /**
+     * Default response timeout.
+     */
     std::chrono::milliseconds response_timeout = 2000ms;
+
+    /**
+     * The period to poll the input buffer at.
+     */
     std::chrono::milliseconds rx_period = 100ms;
+
+    /**
+     * Designates the driver as the master node.
+     */
     bool master = false;
+
+    /**
+     * Callback function for start events.
+     * Parameters: (seconds, nanoseconds, source ID, broadcasted,
+     * sequence count, packet id).
+     */
     std::function<void(int64_t, uint64_t, uint16_t, bool, uint8_t, uint16_t)>
         start_callback = nullptr;
+
+    /**
+     * Callback for heartbeat events.
+     * Parameters: (source ID, ready, broadcasted).
+     */
     std::function<void(uint16_t, bool, bool)> heartbeat_callback = nullptr;
+
+    /**
+     * Callback for claim events.
+     * Parameters: (Master node ID).
+     */
     std::function<void(uint16_t)> claim_callback = nullptr;
+
+    /**
+     * Log event callback.
+     * Parameters: (source ID, Log ID, chunk ID, number of chunks, message).
+     */
     std::function<void(uint16_t, uint16_t, uint8_t, uint8_t,
                        const std::string &)>
         log_callback = nullptr;
 
+    /**
+     * Alpha parameter for the Gamma distribution.
+     */
     double alpha = 1.0;
+
+    /**
+     * Beta parameter for the Gamma distribution.
+     */
     double beta = 2.0;
 };
 
+/**
+ * @struct AresLoraConfig
+ *
+ * LoRa modem configurations.
+ */
 struct AresLoraConfig {
     AresLoraConfig() = default;
+
+    /**
+     * Construct from Python kwargs.
+     * @param kwargs Python keyword arguments.
+     */
     explicit AresLoraConfig(const py::kwargs &kwargs);
 
+    /**
+     * Frequency in Hz to use for transceiving
+     */
     uint32_t frequency = 0;
+
+    /**
+     * Length of the preamble.
+     */
     uint16_t preamble_length = 0;
+
+    /**
+     * The bandwidth to use for transceiving.
+     */
     uint8_t bandwidth = 0;
+
+    /**
+     * The data-rate to use for transceiving.
+     */
     uint8_t datarate = 0;
+
+    /**
+     * The coding rate to use for transceiving.
+     */
     uint8_t coding_rate = 0;
+
+    /**
+     * TX-power in dBm to use for transmission.
+     */
     int8_t tx_power = 0;
 
+    /**
+     * Generate an AresFrame from the object.
+     * @return The frame object generated.
+     */
     [[nodiscard]] AresFrame generate_frame() const;
 };
 
+/**
+ * @class AresSerial
+ *
+ * Serial driver class for communicating with Ares LoRa devices.
+ */
 class AresSerial {
   public:
+    /**
+     * Constructor.
+     * @param configs The configurations for the driver.
+     */
     explicit AresSerial(const AresSerialConfigs &configs);
+
+    /**
+     * Destructor. Automatically stops the driver and closes any serial
+     * connections.
+     */
     ~AresSerial();
 
-    // returns error code
+    /**
+     * Configure a setting on firmware.
+     * @param id The setting ID.
+     * @param value The new value of the setting.
+     * @return The ACK'ed error code from the firmware.
+     */
     int setting_set(uint16_t id, uint32_t value);
-    // returns (value, error code)
+
+    /**
+     * Retrieve a setting configuration from firmware.
+     * @param id The setting ID.
+     * @return py::tuple<setting value, ACK'ed error code>
+     */
     py::tuple setting_get(uint16_t id);
 
-    // returns error code
+    /**
+     * Send a start message over the LoRa network.
+     * @param sec The seconds part of the start time.
+     * @param nsec The nanoseconds part of the start time.
+     * @param id The ID to send the start message to. Ignored if broadcast is
+     * set.
+     * @param broadcast Broadcast the start time to all the listening nodes.
+     * @return The ACK'ed error code from firmware.
+     */
     int send_start(int64_t sec, uint64_t nsec, uint16_t id, bool broadcast);
 
-    // returns error code
+    /**
+     * Configure the LoRa modem.
+     * @param config The new LoRa modem configurations.
+     * @return The ACK'ed error code from the firmware.
+     */
     int lora_config(const AresLoraConfig &config);
 
+    /**
+     * Set the firmware response timeout.
+     * @param timeout The new response timeout.
+     */
     void set_response_timeout(const std::chrono::milliseconds &timeout);
+
+    /**
+     * Retrieve the firmware response timeout.
+     * @return The firmware response timeout.
+     */
     [[nodiscard]] std::chrono::milliseconds get_response_timeout() const;
 
+    /**
+     * Set or retrieve the LED state from firmware.
+     * @param state The new LED state for the firmware or a fetch state request.
+     * @return py::tuple<Fetch LED state | Default LED state, ACK'ed error code>
+     */
     py::tuple led(uint8_t state);
 
-    // returns error code
+    /**
+     * Send a heartbeat message over the LoRa network.
+     * @param ready Flag indicating the system is ready to collect data.
+     * @param tx_cnt The amount of times to transmit the heartbeat.
+     * @return The ACK'ed error code from the firmware.
+     */
     int send_heartbeat(bool ready, uint8_t tx_cnt);
 
-    // returns error code of all frames
+    /**
+     * Send a logging message over the LoRa network.
+     * @param log_msg The log message to send.
+     * @param broadcast Flag indicating if the log message should be
+     * broadcasted.
+     * @param tx_cnt The number of times to send the message or the max
+     * attempts.
+     * @param id The ID to direct the message to.
+     * @return py::tuple<ACK'ed error code, ...>.
+     *
+     * @note If `broadcast` is `true`, `id` is ignored.
+     * @note If `broadcast` is `false`, and id is `0`, then `id` will be
+     * overridden to be the claimed master node. If there is no claimed master
+     * node, then the `broadcast` flag will be overridden to be `true`.
+     * @note `tx_cnt` is the number of times the message is sent when
+     * broadcasting. If not broadcasting, it is the maximum number of attempts
+     * to exchange a logging message chunk with the destination node.
+     */
     py::tuple send_log(const std::string &log_msg, bool broadcast,
                        uint8_t tx_cnt, uint16_t id);
 
+    /**
+     * Retrieve version information from the firmware.
+     * @return py::tuple<app version tuple, ncs version tuple, kernel version
+     * tuple>
+     */
     py::tuple version();
 
+    /**
+     * Set the logging level for the core module.
+     * @param level The new logging level.
+     */
     void set_logging_level(uint32_t level);
 
+    /**
+     * Start driver execution.
+     */
     void start();
+
+    /**
+     * Stop driver execution.
+     */
     void stop();
 
   private:
