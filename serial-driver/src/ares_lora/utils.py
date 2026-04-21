2e8633816cef1d14fc3c6515db2533eefd0b6dab
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-04-21 14:40:19 -0400

    Document utils

--- serial-driver/src/ares_lora/utils.py
+++ serial-driver/src/ares_lora/utils.py
@@ -1,6 +1,5 @@
 import serial.tools.list_ports as list_ports
 
-
 TARGETS = (
     'CMU LoRa',
 )
@@ -29,6 +28,13 @@ def check_serial_port(port: str) -> bool:
 
 
 def find_ports(targets: tuple[str, ...] | None = None) -> dict[str, list[str]]:
+    """Finds valid ports that match the description given by the valid targets tuple.
+
+    Args:
+        targets: The valid targets for ports. This is a tuple of strings described as such: f"{manufacturer} {product}".
+
+    Returns: A dictionary where the key is a valid target and the value is a list of ports associated with that target.
+    """
     if targets is None:
         targets: tuple[str, ...] = TARGETS
     return _find_ports(targets)
