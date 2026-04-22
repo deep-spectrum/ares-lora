34064665f72c93d610533f13e70a09bde487ea00
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-04-22 16:06:52 -0400

    More docs

--- firmware/include/serial/serial_common.h
+++ firmware/include/serial/serial_common.h
@@ -1,7 +1,7 @@
 /**
  * @file serial_common.h
  *
- * @brief
+ * @brief Common serial macros.
  *
  * @date 4/15/26
  *
@@ -12,14 +12,26 @@
 #define ARES_SERIAL_COMMON_H
 
 #if defined(CONFIG_ARES_SERIAL_TX_RINGBUF_LEN)
+/**
+ * Buffer size for the Tx ring buffer.
+ */
 #define SERIAL_BACKEND_TX_RINGBUF_SIZE CONFIG_ARES_SERIAL_TX_RINGBUF_LEN
 #else
+/**
+ * Buffer size for the Tx ring buffer.
+ */
 #define SERIAL_BACKEND_TX_RINGBUF_SIZE 256
 #endif
 
 #if defined(CONFIG_ARES_SERIAL_RX_RINGBUF_LEN)
+/**
+ * Buffer size for the Rx ring buffer.
+ */
 #define SERIAL_BACKEND_RX_RINGBUF_SIZE CONFIG_ARES_SERIAL_RX_RINGBUF_LEN
 #else
+/**
+ * Buffer size for the Rx ring buffer.
+ */
 #define SERIAL_BACKEND_RX_RINGBUF_SIZE 256
 #endif
 
