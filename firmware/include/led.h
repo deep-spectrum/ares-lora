ea1c770af4534651f6504cc8d6b0573e6ff5adbe
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-04-22 10:59:19 -0400

    Write docs for top level

--- firmware/include/led.h
+++ firmware/include/led.h
@@ -1,7 +1,7 @@
 /**
  * @file led.h
  *
- * @brief
+ * @brief LED API.
  *
  * @date 4/6/26
  *
@@ -13,8 +13,28 @@
 
 #include <stdint.h>
 
-enum led_state { OFF, ON, BLINK, LED_INVALID };
+/**
+ * @enum led_state
+ * Different states the LED can be in.
+ */
+enum led_state {
+    OFF,         ///< LED off.
+    ON,          ///< LED on.
+    BLINK,       ///< LED blinking.
+    LED_INVALID, ///< Invalid/end of enumeration.
+};
 
+/**
+ * Update or retrieve the LED state.
+ *
+ * @param[in] new_state The new state of the LED. If invalid, retrieves the
+ * current state.
+ *
+ * @return The current state of the LED if `new_state` is invalid.
+ * @return 0 if new state was set successfully.
+ * @return -EAGAIN if state is unchanged from previous setting.
+ * @return negative error code otherwise.
+ */
 int update_led_state(uint8_t new_state);
 
 #endif // ARES_LED_H
--- firmware/include/settings.h
+++ firmware/include/settings.h
@@ -1,7 +1,7 @@
 /**
  * @file settings.h
  *
- * @brief
+ * @brief Ares-LoRa settings manager.
  *
  * @date 3/27/26
  *
@@ -13,20 +13,71 @@
 
 #include <zephyr/kernel.h>
 
+/**
+ * The settings for Ares-LoRa, their defaults, and bounds.
+ *
+ * - Node ID
+ * - Wait for USB
+ * - Personal Area Network ID
+ * - Repetition count
+ * .
+ *
+ * Format: setting name, default value, minimum value, maximum value.
+ *
+ * @param FUNC The generator macro function.
+ */
 #define FOREACH_ARES_SETTING(FUNC)                                             \
     FUNC(ID, 0, 1, 0xFFFF)                                                     \
     FUNC(WAIT_USB_HOST, 0, 0, 1)                                               \
     FUNC(PANID, 0, 0, 0xFFFF)                                                  \
     FUNC(REPCNT, 10, 1, 0xFFFFFFFF)
 
+/**
+ * Helper for generating the enumerations for Ares-LoRa settings.
+ * @param setting_ The setting name.
+ */
 #define GENERATE_ENUM(setting_, ...) UTIL_CAT(ARES_SETTING_, setting_),
 
-enum ares_setting { FOREACH_ARES_SETTING(GENERATE_ENUM) ARES_SETTING_RESERVED };
+/**
+ * @enum ares_setting
+ *
+ * Represents various settings for Ares-LoRa.
+ */
+enum ares_setting {
+    FOREACH_ARES_SETTING(GENERATE_ENUM)
+        ARES_SETTING_RESERVED ///< Reserved settings enumerator indicating the
+                              ///< last setting.
+};
 
 #undef GENERATE_ENUM
 
+/**
+ * Write a new value for a setting.
+ *
+ * @param[in] setting The setting to update.
+ * @param[in] value The new value of the setting.
+ *
+ * @return 0 on success.
+ * @return -EINVAL if setting is invalid.
+ * @return -ERANGE if the setting is out of range.
+ * @return negative error code otherwise.
+ */
 int update_setting(enum ares_setting setting, uint32_t value);
+
+/**
+ * Read the given setting value.
+ *
+ * @param[in] setting The setting to read.
+ * @param[out] value A pointer to the container for the setting.
+ *
+ * @return 0 on success.
+ * @return -EINVAL if setting is invalid or `value` is NULL.
+ */
 int retrieve_setting(enum ares_setting setting, uint32_t *value);
+
+/**
+ * Resets all the settings to their default values.
+ */
 void reset_settings(void);
 
 #endif // ARES_SETTINGS_H
