defebf7171fe947e0c19dff10b110fffea839974
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-07-31 15:52:57 -0400

    Write lora config frame payload

--- serial-driver/core/include/ares-lora-serial/frames/mcu/lora_config.hpp
+++ serial-driver/core/include/ares-lora-serial/frames/mcu/lora_config.hpp
@@ -11,4 +11,27 @@
 #ifndef ARES_LORA_CONFIG_HPP
 #define ARES_LORA_CONFIG_HPP
 
+#include <ares-lora-serial/frames/payload_base.hpp>
+
+namespace AresFrame {
+struct LoraConfig : Internal::FramePayloadBase {
+    uint32_t frequency = 0;
+    uint16_t preamble_length = 0;
+    uint8_t bandwidth;
+    uint8_t data_rate = 0;
+    uint8_t coding_rate = 0;
+    int8_t tx_power = 0;
+    uint8_t cad_mode = 0;
+    uint8_t cad_num_symbols = 0;
+    uint8_t cad_det_peak = 0;
+    uint8_t cad_det_min = 0;
+
+    std::size_t payload_size() override;
+    void serialize(std::vector<uint8_t> &buffer) override;
+
+  private:
+    static constexpr std::size_t _payload_size = 14;
+};
+} // namespace AresFrame
+
 #endif // ARES_LORA_CONFIG_HPP
--- serial-driver/core/include/ares-lora-serial/frames/payload_base.hpp
+++ serial-driver/core/include/ares-lora-serial/frames/payload_base.hpp
@@ -12,23 +12,55 @@
 #define ARES_PAYLOAD_BASE_HPP
 
 #include <cstdint>
+#include <exception>
+#include <string>
 #include <vector>
 
-namespace AresFrame::Internal {
-struct FramePayloadBase {
-    virtual ~FramePayloadBase() = default;
+namespace AresFrame {
 
-    virtual std::size_t payload_size() = 0;
+class NotSupported : std::exception {
+  public:
+    explicit NotSupported(const char *msg) : _msg(msg) {}
+
+    [[nodiscard]] const char *what() const noexcept override {
+        return _msg.c_str();
+    }
+
+  private:
+    const std::string _msg;
+};
+
+namespace Internal {
+struct FramePayloadBase {
+    virtual ~FramePayloadBase() = 0;
+
+    virtual std::size_t payload_size() {
+        throw NotSupported("Payload size is not supported in RX only frames.");
+    }
+
     virtual void preprocess() {
         // default behavior is no preprocessing necessary.
+        // recommended uses:
+        // - validation of parameters
+        // - Splitting up a message to be sent over multiple frames.
     }
-    virtual void serialize(std::vector<uint8_t> &buffer) = 0;
-    virtual void deserialize(const uint8_t *buffer, std::size_t len) = 0;
+
+    virtual void serialize(std::vector<uint8_t> &buffer) {
+        throw NotSupported("serialize() is not supported in RX only frames.");
+    }
+
+    virtual void deserialize(const uint8_t *buffer, std::size_t len) {
+        throw NotSupported("deserialize() is not supported in TX only frames.");
+    }
+
     virtual bool new_frame() {
         // default behavior is single frame is needed.
         return false;
     }
 };
-} // namespace AresFrame::Internal
+
+inline FramePayloadBase::~FramePayloadBase() = default;
+} // namespace Internal
+} // namespace AresFrame
 
 #endif // ARES_PAYLOAD_BASE_HPP
