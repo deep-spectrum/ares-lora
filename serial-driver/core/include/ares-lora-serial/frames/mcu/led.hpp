793f4f1e4e90ea6b142502ec65e948b638861f0d
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-07-31 15:59:33 -0400

    led

--- serial-driver/core/include/ares-lora-serial/frames/mcu/led.hpp
+++ serial-driver/core/include/ares-lora-serial/frames/mcu/led.hpp
@@ -11,4 +11,40 @@
 #ifndef ARES_LED_HPP
 #define ARES_LED_HPP
 
+#include <ares-lora-serial/frames/payload_base.hpp>
+
+namespace AresFrame {
+struct Led : Internal::FramePayloadBase {
+    /**
+     * @enum LedState
+     *
+     * LED states.
+     */
+    enum LedState : uint8_t {
+        OFF = 0,   ///< LED off.
+        ON = 1,    ///< LED on.
+        BLINK = 2, ///< LED blinking at 1 Hz.
+        FADE = 3,  ///< LED fading.
+        FETCH = 4, ///< Retrieve LED state from firmware.
+    };
+
+    /**
+     * The LED number/ID.
+     */
+    uint8_t led = 0;
+
+    /**
+     * The LED state frame data.
+     */
+    LedState state = FETCH;
+
+    std::size_t payload_size() override;
+    void serialize(std::vector<uint8_t> &buffer) override;
+    void deserialize(const uint8_t *buffer, std::size_t len) override;
+
+  private:
+    static constexpr std::size_t _payload_size = 2;
+};
+} // namespace AresFrame
+
 #endif // ARES_LED_HPP
---
+++ serial-driver/core/src/ares_lora_serial/frames/mcu/led.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file led.cpp
+ *
+ * @brief
+ *
+ * @date 7/31/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+#include <ares-lora-serial/frames/mcu/led.hpp>
+#include <ares/serialization.hpp>
+
+namespace AresFrame {
+std::size_t Led::payload_size() { return _payload_size; }
+
+void Led::serialize(std::vector<uint8_t> &buffer) {
+    ares::serialize(buffer, led, state);
+}
+
+void Led::deserialize(const uint8_t *buffer, std::size_t len) {
+    if (len != _payload_size) {
+        // todo
+    }
+
+    ares::deserialize(buffer, led, state);
+}
+} // namespace AresFrame
