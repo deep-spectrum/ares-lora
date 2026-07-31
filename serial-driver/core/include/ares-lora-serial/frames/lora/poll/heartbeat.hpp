3b9c6f11607d2853e746052976610e9ff6a259a5
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-07-31 16:15:25 -0400

    heartbeat

--- serial-driver/core/include/ares-lora-serial/frames/lora/poll/heartbeat.hpp
+++ serial-driver/core/include/ares-lora-serial/frames/lora/poll/heartbeat.hpp
@@ -11,4 +11,20 @@
 #ifndef ARES_HEARTBEAT_HPP
 #define ARES_HEARTBEAT_HPP
 
+#include <ares-lora-serial/frames/payload_base.hpp>
+
+namespace AresFrame {
+struct Heartbeat : Internal::FramePayloadBase {
+    bool ready = false;
+    uint16_t id = 0;
+
+    std::size_t payload_size() override;
+    void serialize(std::vector<uint8_t> &buffer) override;
+    void deserialize(const uint8_t *buffer, std::size_t len) override;
+
+  private:
+    static constexpr std::size_t _payload_size = 3;
+};
+} // namespace AresFrame
+
 #endif // ARES_HEARTBEAT_HPP
